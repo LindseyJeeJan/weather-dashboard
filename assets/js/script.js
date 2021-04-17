f7e6bcdbe3d5b45ce01f5ec6381f7769cb915e02
Lindsey JJ <ljjlauria@mac.com>
2021-04-16 21:11:10 -0700

    added items returned to search history list. Display the API location names on the page header

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,13 +9,13 @@ var windSpeed = $('#wind-speed');
 var uvIndex = $('#uv-index .badge');
 var weatherIcon = $('#h2-card-image');
 var errorMessage = $('#error');
+var errorCitySearch = $('#error-city');
 
 var latLong = [];
 
 // Click event to capture when user clicks search
 btnSearch.on('click', function(event) {
     event.preventDefault();
-    errorMessage.hide();
     var searchLocation = searchInput.val().trim();
     if (searchLocation === ""){
         // return if search empty
@@ -23,31 +23,54 @@ btnSearch.on('click', function(event) {
         return;
     }
     // function to retrieve the longitude and latitude of the city searched
-    var coordinates = convertToLongLat(searchLocation);
+    convertToLongLat(searchLocation);
 });
 
 // Click event to capture user clicking on a city
 cityList.on('click', 'li', function(event) {
+     // change background of selected city 
     var listLocation = $(this).attr('data-location');
-
     $('li').removeClass('bg-primary');
     $(this).addClass('bg-primary');
     // function to retrieve the longitude and latitude of the city clicked
     convertToLongLat(listLocation);
 });
 
+function resetErrorMessages() {
+    errorMessage.hide();
+    errorCitySearch.hide();
+}
+
 // Convert the city name to long and lat for the Open API
-function convertToLongLat(cityName){
+function convertToLongLat(city){
     var requestUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' +
-    cityName + '&limit=5&appid=ce4222a2bf38275175e19449b4ee48a5';
+    city + '&limit=5&appid=ce4222a2bf38275175e19449b4ee48a5';
     fetch(requestUrl)
         .then(function (response) {
+            // If city information is not found, instruct the user to enter a new
+            if (response.status === 404) {
+                console.log('404');
+                errorCitySearch.show();
+                return;
+            } 
             return response.json();
         })
         .then(function (data) {
-            // store longitude and latitude in array
-            latLong.push(data[0].lat);
-            latLong.push(data[0].lon);
+            console.log(data);
+            if ((data[0].country) == "US") {
+                var cityStateCountry = (data[0].local_names.en + ', ' + data[0].state);
+            } else {
+                var cityStateCountry = (data[0].local_names.en + ', ' + data[0].country);
+            }
+            // Add item to search history
+            var newListItem = $('<li class="list-group-item">');
+            newListItem.text(cityStateCountry);
+            cityList.append(newListItem);
+            // Display location city, state (if US) and country
+            cityName.text(cityStateCountry);
+            // store longitude and latitude in array, but empty it first
+            latLong[0] = data[0].lat;
+            latLong[1] = data[0].lon;
             // retrieve weather data for this city from the API
             getApi(latLong);
         })
@@ -57,15 +80,22 @@ function convertToLongLat(cityName){
 function getApi(locationCoordinates) {
     var latitudeVal = locationCoordinates[0];
     var longitudeVal = locationCoordinates[1];
-  var requestUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + latitudeVal + '&lon=' + longitudeVal + '&exclude=minutely,hourly,alerts&units=imperial&appid=ce4222a2bf38275175e19449b4ee48a5';
+    var requestUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + latitudeVal + '&lon=' + longitudeVal + '&exclude=minutely,hourly,alerts&units=imperial&appid=ce4222a2bf38275175e19449b4ee48a5';
   fetch(requestUrl)
     .then(function (response) {
+        console.log(response.status);
+        if (response.status === 404) {
+            // If city information is not found, instruct the user to enter a new
+            errorCitySearch.show();
+        }
+        if (response.status === 200) {
+            resetErrorMessages();
+        }
       return response.json();
     })
     .then(function (data) {
         // Write information to the main weather card with data from API
         console.log(data);
-        cityName.text(data.timezone);
         humidity.text(data.current.humidity);
         windSpeed.text(data.current.wind_speed);
         uvIndex.text(data.current.uvi);
@@ -91,10 +121,6 @@ function getApi(locationCoordinates) {
         var temp = data.current.temp;
         temp = parseFloat(temp).toFixed(1);
         temperature.text(temp);
-
-        // TODO: determine if own icon will be used or pull one from the API
-        var condition = data.current.weather[0].main;
-        console.log('condition', condition);
 
         // Store the array of forecasts
         var forecasts = data.daily;
